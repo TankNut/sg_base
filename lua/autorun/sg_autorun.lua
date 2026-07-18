ab1e9e39bc40be1e64acc77cfa8f16fe9c4d661a
TankNut <m.vlasblom@live.nl>
2026-07-18 17:16:56 +0200

    Rework sg.Tracer to use a callback setup

--- lua/autorun/sg_autorun.lua
+++ lua/autorun/sg_autorun.lua
@@ -3,11 +3,10 @@ module("sg", package.seeall)
 InfiniteAmmo = CreateConVar("sg_infiniteammo", 0, {FCVAR_ARCHIVE, FCVAR_REPLICATED})
 
 if CLIENT then
-	local tracerColor = Color(255, 255, 255)
 	local dir = Vector()
 
 	-- Returns false if done rendering
-	function Tracer(startpos, endpos, velocity, length, scale, time, color)
+	function Tracer(startpos, endpos, velocity, length, time, callback)
 		dir:Set(endpos)
 		dir:Sub(startpos)
 
@@ -32,31 +31,14 @@ if CLIENT then
 		startDistance = math.Clamp(startDistance, 0, distance)
 		endDistance = math.Clamp(endDistance, 0, distance)
 
-		if startDistance == 0 and endDistance == 0 then
-			return true
-		end
-
-		local offset = math.abs(startDistance - endDistance) / length
-
-		local origin = EyePos()
-
 		-- Is this backwards? I don't know
-		local endPoint = startpos + dir * startDistance
-		local startPoint = startpos + dir * endDistance
+		local startPoint = startpos + dir * startDistance
+		local endPoint = startpos + dir * endDistance
 
-		local lineDir = endPoint - startPoint
-		local viewDir = endPoint - origin
+		local uv1 = math.abs(startDistance - endDistance) / length
+		local uv2 = 0
 
-		local cross = lineDir:Cross(viewDir)
-		cross:Normalize()
-
-		tracerColor:Set(color or color_white)
-
-		render.DrawBeam(startPoint, endPoint, scale * 2, 0, offset, tracerColor)
-
-		tracerColor:SetBrightness(0.25)
-
-		render.DrawBeam(startPoint, endPoint, scale * 4, 0, offset, tracerColor)
+		callback(startPoint, endPoint, uv1, uv2)
 
 		return true
 	end

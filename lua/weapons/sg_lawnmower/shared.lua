900b6a4c6fc32aa2ac3d3960489d9cddbdefcf06
Grixis <john.p.scheri@gmail.com>
2026-07-25 19:13:34 -0400

    Added quads for little screen and keypad

--- lua/weapons/sg_lawnmower/shared.lua
+++ lua/weapons/sg_lawnmower/shared.lua
@@ -79,7 +79,7 @@ if CLIENT then
 
 	surface.CreateFont("SG_Lawnmower_Ammo2", {
 		font = "Trebuchet MS",
-		size = 100,
+		size = 250,
 		weight = 900,
 		antialias = true,
 		scanlines = 4,
@@ -88,15 +88,19 @@ if CLIENT then
 	})
 
 	local ammoColor = Color(255, 255, 0)
-
+	local ammoColor2 = Color(200, 200, 200)
+	
 	function SWEP:DrawAmmoCounter()
 		local fraction = self:Clip1() / self:GetMaxClip1()
 
 		ammoColor.r = (1 - fraction) * 255
 		ammoColor.g = fraction * 255
 
-		draw.SimpleText(self:Clip1(), "SG_Lawnmower_Ammo", 0, -50, ammoColor, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-		draw.SimpleText(self:Ammo1(), "SG_Lawnmower_Ammo2", 0, 100, ammoColor, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+		draw.SimpleText(self:Clip1(), "SG_Lawnmower_Ammo", 0, 0, ammoColor, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+	end
+	
+	function SWEP:DrawReserveCounter()
+		draw.SimpleText(self:Ammo1(), "SG_Lawnmower_Ammo2", 0, 0, ammoColor2, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
 	end
 end
 

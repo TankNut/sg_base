912411eb3ba687eda0e628ac8af4f745531f2f9e
Grixis <john.p.scheri@gmail.com>
2026-08-04 20:57:55 -0400

    Added the Quadruple Agent

---
+++ lua/weapons/sg_quad_agent/shared.lua
@@ -0,0 +1,82 @@
+AddCSLuaFile()
+DEFINE_BASECLASS("sg_gun_base")
+
+SWEP.Base = "sg_gun_base"
+
+SWEP.PrintName = "The Quadruple Agent"
+SWEP.Category = "S&G Munitions"
+
+SWEP.Instructions = "Pull the trigger to betray people."
+SWEP.Purpose = "To trick your enemies into thinking that your gun is both on their side and betraying you, when it's really the opposite."
+
+SWEP.Slot = 1
+
+SWEP.Spawnable = true
+
+-- Ammo
+SWEP.Primary.Ammo = "357"
+SWEP.Primary.ClipSize = 6
+SWEP.Primary.DefaultClip = 32
+
+-- HoldType
+SWEP.HoldType = "revolver"
+
+-- Firemode
+SWEP.Firemode = 1
+
+-- Balance
+SWEP.AmmoCost = 1
+SWEP.Count = 1
+SWEP.Damage = 35
+
+SWEP.Accuracy = 10
+SWEP.Range = 1000
+
+SWEP.Delay = 0.45
+
+-- Recoil
+SWEP.Recoil = {
+	Min = Angle(0.5, 0.25),
+	Max = Angle(1, -0.25)
+}
+
+SWEP.RecoilAdd = 0.5
+SWEP.ViewPunch = 1
+SWEP.RecoilFlip = false
+
+-- Effects
+SWEP.Tracer = 1
+SWEP.TracerName = "sg_e_tracer"
+SWEP.TracerConfig = {}
+
+include("sh_model.lua")
+
+function SWEP:OnPrimaryAnimation()
+	self:EmitSound("Weapon_SG_QuadAgent.Single1")
+	self:EmitSound("Weapon_SG_QuadAgent.Single2")
+end
+
+function SWEP:OnReloadAnimation()
+	self:EmitSound("")
+end
+
+sound.Add({
+	name = "Weapon_SG_QuadAgent.Single1",
+	channel = CHAN_WEAPON,
+	volume = 1,
+	level = sg.LEVEL_GUNFIRE,
+	pitch = {150, 165},
+	sound = ")weapons/357/357_fire2.wav"
+})
+sound.Add({
+	name = "Weapon_SG_QuadAgent.Single2",
+	channel = CHAN_ITEM,
+	volume = 1,
+	level = sg.LEVEL_GUNFIRE,
+	pitch = {140, 145},
+	sound = ")weapons/p228/p228-1.wav"
+})
+
+--[[Grixis Notes: Still working on balancing this so values may look fucky. This one won't have breech loading in the end but 
+feel free to use this as a test base for that if needed along with scoping once we get there too. Also, noticed at least based
+on what I saw in the wiki and the sounds list from hl2_game_sounds_weapons that the 357 utilizes multiple sound scripts?]]--

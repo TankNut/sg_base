59520a555d573e3dec79790ed282784fa84c12d4
TankNut <m.vlasblom@live.nl>
2026-08-08 00:02:56 +0200

    Add a cartoony option to the SelfKnockback trait

--- lua/sg/traits/self_knockback.lua
+++ lua/sg/traits/self_knockback.lua
@@ -1,11 +1,16 @@
 local TRAIT = {}
 
 TRAIT.Force = 1000 -- A reminder to put in your own config
+TRAIT.Cartoony = false -- If enabled, applies knockback when crouching and while in the air
 
 function TRAIT:Hook_PostFireWeapon(ent)
 	local ply = ent:GetOwner()
 
 	if ply:IsPlayer() and IsFirstTimePredicted() then
+		if not self.Cartoony and (ply:Crouching() or not ply:IsOnGround()) then
+			return
+		end
+
 		ply:SetVelocity(ent:GetShootDir() * -self.Force)
 	end
 end

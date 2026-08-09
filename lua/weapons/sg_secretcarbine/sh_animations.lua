65999059a3d7441719203a842c69339698d271ca
TankNut <m.vlasblom@live.nl>
2026-08-10 01:23:32 +0200

    Update sh_animations.lua

--- lua/weapons/sg_secretcarbine/sh_animations.lua
+++ lua/weapons/sg_secretcarbine/sh_animations.lua
@@ -1,7 +1,5 @@
 AddCSLuaFile()
 
--- This is my animation testing gun at the moment, so expect this to be a MESS. Other weapons will be cleaner I promise
-
 local reload = sg.Animation.WeaponSequence(ACT_VM_RELOAD)
 reload:AddEvent(0, "PlayerAnimation", PLAYER_RELOAD)
 reload:AddEvent(1 / 48, "Sound", "Weapon_AR2.Reload_Rotate")
@@ -20,32 +18,9 @@ if CLIENT then
 		[36 / 48] = {pos = lowerPos, angle = lowerAng, {angle = false}},
 		[46 / 48] = {pos = defaultPos, angle = defaultAng},
 	})
-
-	-- reload:AddVElementLayer("screen", {
-	-- 	[0] = {pos2 = Vector(), color = Color(255, 0, 0)},
-	-- 	[0.5] = {pos2 = Vector(0, 0, 5)},
-	-- 	[1] = {pos2 = Vector(), color = color_white}
-	-- })
-
-	-- reload:AddVElementLayer("screen", function(self, element, cycle)
-	-- 	-- Full rotation in 1 cycle
-	-- 	local rad = cycle * math.pi * 2
-
-	-- 	local sine = -math.sin(rad) * 2
-	-- 	local cose = -math.cos(rad) * 2
-
-	-- 	return {
-	-- 		pos2 = Vector(0, sine, 2 + cose),
-	-- 		angle2 = Angle(0, 0, -cycle * 360)
-	-- 	}
-	-- end)
-
-	reload:AddElementLayer("Laser", function(self, element, cycle)
-		return {
-			color = HSVToColor(cycle * 360, 1, 1)
-		}
-	end)
 end
+
+
 
 SWEP.Animations = {
 	Reload = reload

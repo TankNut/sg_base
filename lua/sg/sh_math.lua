210f46551850036f73cebeabccca3167d80d367c
TankNut <m.vlasblom@live.nl>
2026-08-15 00:57:03 +0200

    Unwrap the math functions

--- lua/sg/sh_math.lua
+++ lua/sg/sh_math.lua
@@ -1,15 +1,13 @@
 module("sg", package.seeall)
 
+-- Combines math.Remap with math.Clamp
 function RemapC(val, inMin, inMax, outMin, outMax)
-	return math.Clamp(math.Remap(val, inMin, inMax, outMin, outMax), math.min(outMin, outMax), math.max(outMin, outMax))
+	local fraction = math.min(math.max((val - inMin) / (inMax - inMin), 0), 1)
+
+	return outMin + (outMax - outMin) * fraction
 end
 
-function RemapTime(start, finish, from, to)
-	local fraction = math.Clamp(math.TimeFraction(start, finish, CurTime()), 0, 1)
-
-	return Lerp(fraction, from, to)
-end
-
+-- Clamps math.TimeFraction to 0-1
 function TimeFraction(start, finish)
-	return math.Clamp(math.TimeFraction(start, finish, CurTime()), 0, 1)
+	return math.min(math.max((CurTime() - start) / (finish - start), 0), 1)
 end

1975713f7f325d4950fde8e52f32a389e18e14cc
Grixis <john.p.scheri@gmail.com>
2026-09-07 08:47:59 -0400

    Reverted muzzle

--- lua/weapons/sg_secretcarbine/shared.lua
+++ lua/weapons/sg_secretcarbine/shared.lua
@@ -63,10 +63,8 @@ SWEP.TracerConfig = {
 	Scale = {0.5, 1.5}
 }
 
-SWEP.MuzzleEffect = "sg_e_muzzle_smoke"
-SWEP.MuzzleConfig = {
-	Scale = 2
-}
+SWEP.MuzzleEffect = "sg_e_muzzle_ar2"
+SWEP.MuzzleConfig = {}
 
 include("sh_model.lua")
 include("sh_animations.lua")
